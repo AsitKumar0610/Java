881b5ba577f6d8cbe7fd63022d91d0012a516509
AsitKumar0610 <77540139+AsitKumar0610@users.noreply.github.com>
2022-05-16 19:54:20 +0530

    Add files via upload

---
+++ using packages/lab.java
@@ -0,0 +1,13 @@
+package P1 ;
+public class lab 
+{
+    public int sum (int a, int b )
+    {
+        return a+b ;
+    }
+    public float div(float a, float b)
+    {
+        float c = a/b ;
+        return c ;
+    }
+}
---
+++ using packages/test.java
@@ -0,0 +1,13 @@
+package P2 ;
+import P1.* ;
+public class test extends lab implements I1
+{
+    return a*b ;
+}
+public static void main (String args[])
+{
+    test obj = new test();
+    System.out.println(obj.sum(6,12));
+    System.out.println(obj.mul(6,12));
+}
+}
